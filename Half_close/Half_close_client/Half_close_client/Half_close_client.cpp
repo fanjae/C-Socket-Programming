68180f4656000f484669eaaf13e8af87e36942c8
fanjaekim <hjjang45@gmail.com>
2021-08-04 14:49:56 +0900

    윈도우 소켓프로그래밍 Half_Close Example
    윈도우 소켓프로그래밍 Half_Close Example

---
+++ Half_close/Half_close_client/Half_close_client/Half_close_client.cpp
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <WinSock2.h>
+
+#define BUF_SIZE 30
+#pragma warning (disable:4996)
+#pragma comment (lib,"ws2_32.lib")
+void Handle_error(const char* message);
+
+int main(int argc, char * argv[])
+{
+	WSADATA wsaData;
+	SOCKET serv_socket;
+	FILE *fp;
+
+	char buf[BUF_SIZE];
+	int readCnt;
+	SOCKADDR_IN serv_addr;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		Handle_error("WSAStartup() Error!");
+	}
+
+	fp = fopen("receive.dat", "wb");
+	serv_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	connect(serv_socket, (SOCKADDR*)&serv_addr, sizeof(serv_addr));
+
+	while ((readCnt = recv(serv_socket, buf, BUF_SIZE, 0)) != 0)
+	{
+		fwrite((void *)buf, 1, readCnt, fp);
+	}
+
+	puts("Received file data");
+	send(serv_socket, "Thank You", 10, 0);
+	fclose(fp);
+	closesocket(serv_socket);
+	WSACleanup();
+	return 0;
+}
+
+void Handle_error(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ Half_close/Half_close_server/Half_close_server/Half_close_server.cpp
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <WinSock2.h>
+
+#define BUF_SIZE 30
+#pragma warning (disable:4996) // fopen 에러 무시
+#pragma comment (lib,"ws2_32.lib")
+void Handle_Error(const char *message);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET serversocket, clientsocket;
+	FILE *fp;
+
+	char buf[BUF_SIZE];
+	int readCnt;
+
+	SOCKADDR_IN server_addr, client_addr;
+	int client_addr_size;
+
+	if (argc != 2)
+	{
+		printf("Usage : %s <port> \n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		Handle_Error("WSAStartup() error!");
+	}
+
+	fp = fopen("Half_close_server.cpp", "rb");
+	serversocket = socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(atoi(argv[1]));
+
+	bind(serversocket, (SOCKADDR*)&server_addr, sizeof(server_addr));
+	listen(serversocket, 5);
+
+	client_addr_size = sizeof(client_addr);
+	clientsocket = accept(serversocket, (SOCKADDR*)&client_addr, &client_addr_size);
+
+	while (1)
+	{
+		readCnt = fread((void *)buf, 1, BUF_SIZE, fp);
+		if (readCnt < BUF_SIZE)
+		{
+			send(clientsocket, (char *)&buf, readCnt, 0);
+			break;
+		}
+		send(clientsocket, (char *)&buf, BUF_SIZE, 0);
+	}
+
+	shutdown(clientsocket, SD_SEND); // 출력 스트림 종료
+	recv(clientsocket, (char *)buf, BUF_SIZE, 0);
+	printf("Message from client: %s \n", buf);
+
+	fclose(fp);
+	closesocket(clientsocket);
+	closesocket(serversocket);
+	WSACleanup();
+	return 0;
+}
+
+void Handle_Error(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
