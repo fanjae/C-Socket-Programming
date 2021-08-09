8f8dbe28c2d2c8115c7a399e489bc318aa370e53
fanjaekim <hjjang45@gmail.com>
2021-08-09 16:20:10 +0900

    윈도우 소켓 프로그래밍 Multicast Example

---
+++ multicast_win/news_receiver_win/news_receiver_win/news_receiver_win.cpp
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h> // for struct ip_mreq
+
+#pragma comment (lib,"ws2_32.lib")
+#pragma warning (disable:4996)
+#define BUF_SIZE 30
+void Handle_Error(const char *message);
+
+int main(int argc, char * argv[])
+{
+	WSADATA wsaData;
+	SOCKET recvsock;
+	SOCKADDR_IN addr;
+	struct ip_mreq join_adr;
+	char buf[BUF_SIZE];
+	int str_length;
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <GroupIP> <PORT>\n", argv[0]);
+		exit(1);
+	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		Handle_Error("WSAStartup() error!");
+	}
+
+	recvsock = socket(PF_INET, SOCK_DGRAM, 0);
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(atoi(argv[2]));
+	if (bind(recvsock, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+	{
+		Handle_Error("bind() error");
+	}
+
+	join_adr.imr_multiaddr.s_addr = inet_addr(argv[1]);
+	join_adr.imr_interface.s_addr = htonl(INADDR_ANY);
+	if (setsockopt(recvsock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&join_adr, sizeof(join_adr)) == SOCKET_ERROR)
+	{
+		Handle_Error("setsocket() error");
+	}
+
+	while (1)
+	{
+		str_length = recvfrom(recvsock, buf, BUF_SIZE - 1, 0, NULL, 0);
+		if (str_length < 0)
+		{
+			break;
+		}
+		buf[str_length] = 0;
+		fputs(buf, stdout);
+	}
+	closesocket(recvsock);
+	WSACleanup();
+	return 0;
+}
+void Handle_Error(const char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ multicast_win/news_sender_win/news_sender_win/news_sender_win.cpp
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h> // IP_MULTICAST_TTL 선언
+
+#pragma comment (lib,"ws2_32.lib")
+#pragma warning (disable:4996)
+#define TTL 64
+#define BUF_SIZE 30
+
+void Handle_Error(const char *message);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET sendsock;
+	SOCKADDR_IN mul_addr;
+	int timeLive = TTL;
+	FILE *fp;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <GroupIP> <PORT>\n", argv[0]);
+		exit(1);
+	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		Handle_Error("WSAStartup() error!");
+	}
+
+	sendsock = socket(PF_INET, SOCK_DGRAM, 0);
+	memset(&mul_addr, 0, sizeof(mul_addr));
+	mul_addr.sin_family = AF_INET;
+	mul_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	mul_addr.sin_port = htons(atoi(argv[2]));
+
+	setsockopt(sendsock, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&timeLive, sizeof(timeLive));
+
+	if ((fp = fopen("news.txt", "r")) == NULL)
+	{
+		Handle_Error("fopen() error");
+	}
+	while (!feof(fp))
+	{
+		fgets(buf, BUF_SIZE, fp);
+		sendto(sendsock, buf, strlen(buf), 0, (SOCKADDR*)&mul_addr, sizeof(mul_addr));
+		Sleep(2000);
+	}
+	closesocket(sendsock);
+	WSACleanup();
+	return 0;
+}
+
+void Handle_Error(const char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
