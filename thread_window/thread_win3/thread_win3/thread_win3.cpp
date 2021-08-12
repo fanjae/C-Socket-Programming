9e3b1eb70991d69af82e2418042a4fbf76407900
fanjaekim <hjjang45@gmail.com>
2021-08-12 16:01:47 +0900

    윈도우 소켓 프로그래밍 Thread3 Example

---
+++ thread_window/thread_win3/thread_win3/thread_win3.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <process.h>
+
+/* 본 예제는 동기화가 되어있지 않아
+의도한 대로 프로그램이 실행되지 않음. */
+#define NUM_THREAD 50
+unsigned WINAPI threadInc(void * arg);
+unsigned WINAPI threadDes(void * arg);
+long long int num = 0;
+
+int main(int argc, char * argv[])
+{
+	HANDLE tHandle[NUM_THREAD];
+	int i;
+
+	printf("sizeof long long : %d\n", sizeof(long long));
+	for (i = 0; i < NUM_THREAD; i++)
+	{
+		if (i % 2 == 0)
+		{
+			tHandle[i] = (HANDLE)_beginthreadex(NULL, 0, threadInc, NULL, 0, NULL);
+		}
+		else
+		{
+			tHandle[i] = (HANDLE)_beginthreadex(NULL, 0, threadDes, NULL, 0, NULL);
+		}
+	}
+	// 각 쓰레드마다 5천만번의 덧셈과 뺄셈의 과정을 끝내길 기다림.
+	WaitForMultipleObjects(NUM_THREAD, tHandle, TRUE, INFINITE);
+	printf("result : %lld \n", num);
+	return 0;
+}
+
+unsigned WINAPI threadInc(void *arg)
+{
+	int i;
+	for (i = 0; i < 50000000; i++)
+	{
+		num += i;
+	}
+	return 0;
+}
+unsigned WINAPI threadDes(void *arg)
+{
+	int i;
+	for (i = 0; i < 50000000; i++)
+	{
+		num -= i;
+	}
+	return 0;
+}
