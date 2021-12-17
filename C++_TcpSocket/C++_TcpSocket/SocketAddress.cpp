61aa90bdcc6f7e96601feca650bcd00a7e3db98d
fanjaekim <hjjang45@gmail.com>
2021-12-17 23:51:41 +0900

    TCpSocket Class 구현 - SocketUtil.cpp 내용 추가 중

--- C++_TcpSocket/C++_TcpSocket/SocketAddress.cpp
+++ C++_TcpSocket/C++_TcpSocket/SocketAddress.cpp
@@ -1,2 +1,37 @@
 #include "RoboCatShared.h"
 
+
+bool SocketUtil::StaticInit()
+{
+#if _WIN32
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+	{
+		ReportError("Starting Up");
+		return false;
+	}
+#endif
+	return true;
+}
+
+void SocketUtil::CleanUp()
+{
+#if _WIN32
+	WASCleanup();
+#endif
+}
+
+void SocketUtil::ReportError(const char* inOperationDesc)
+{
+#if _WIN32
+	LPVOID lpMsgBuf;
+	DWORD errorNum = GetLastError();
+
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, errorNum, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+
+	LOG("Error %s: %d- %s", inOperationDesc, errorNum, lpMsgBuf);
+#else
+	LOG("Error : %hs", inOperationDesc);
+#endif
+}
