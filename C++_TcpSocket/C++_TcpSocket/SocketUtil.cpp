6506b84a35fd0a0ac2cdea6698cf6be839dfb426
fanjaekim <hjjang45@gmail.com>
2021-12-18 23:55:53 +0900

    TCPSocket Class 구현 - SocketUtil.cpp 내용 추가

--- C++_TcpSocket/C++_TcpSocket/SocketUtil.cpp
+++ C++_TcpSocket/C++_TcpSocket/SocketUtil.cpp
@@ -36,3 +36,43 @@ void SocketUtil::ReportError(const char* inOperationDesc)
 #endif
 }
 
+int SocketUtil::GetLastError()
+{
+#if _WIN32
+	return WSAGetLastError();
+#else
+	return errno;
+#endif
+}
+
+UDPSocketPtr SocketUtil::CreateUDPSocket(SocketAddressFamily inFamily)
+{
+	SOCKET s = socket(inFamily, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (s != INVALID_SOCKET)
+	{
+		return UDPSocketPtr(new UDPSocket(s));
+	}
+	else
+	{
+		ReportError("SocketUtil::CreateUDPSocket");
+		return nullptr;
+	}
+}
+
+TCPSocketPtr SocketUtil::CreateTCPSocket(SocketAddressFamily inFamily)
+{
+	SOCKET s = socket(inFamily, SOCK_STREAM, IPPROTO_TCP);
+
+	if (s != INVALID_SOCKET)
+	{
+		return TCPSocketPtr(new TCPSocket(s));
+	}
+	else
+	{
+		ReportError("SocketUtil::CreateTCPSocket");
+		return nullptr;
+	}
+}
+
+
