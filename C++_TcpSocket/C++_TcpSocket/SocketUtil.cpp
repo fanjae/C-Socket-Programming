dd102e391da4f6e0502acd797605919c42600d2b
fanjaekim <hjjang45@gmail.com>
2021-12-19 22:50:08 +0900

    TCPSocket 클래스 구현 - SocketUtil.cpp 내용 추가

--- C++_TcpSocket/C++_TcpSocket/SocketUtil.cpp
+++ C++_TcpSocket/C++_TcpSocket/SocketUtil.cpp
@@ -95,3 +95,40 @@ fd_set* SocketUtil::FillSetFromVector(fd_set& outSet, const vector<TCPSocketPtr>
 	}
 }
 
+void SocketUtil::FillVectorFromSet(vector < TCPSocketPtr >* outSockets, const vector < TCPSocketPtr >* inSockets, const fd_set &inSet)
+{
+	if (inSockets && outSockets)
+	{
+		outSockets->clear();
+		for (const TCPSocketPtr& socket : *inSockets)
+		{
+			if (FD_ISSET(socket->mSocket, &inSet))
+			{
+				outSockets->push_back(socket);
+			}
+		}
+	}
+}
+
+int SocketUtil::Select(const vector <TCPSocketPtr> * inReadSet, vector<TCPSocketPtr> *outReadSet, const vector<TCPSocketPtr> *inWriteSet, vector<TCPSocketPtr> *outWriteSet, const vector<TCPSocketPtr> *inExceptSet, vector<TCPSocketPtr> *outExceptSet)
+{
+	fd_set read, write, except;
+
+	int nfds = 0;
+
+	fd_set *readPtr = FillSetFromVector(read, inReadSet, nfds);
+	fd_set *writePtr = FillSetFromVector(write, inWriteSet, nfds);
+	fd_set *exceptPtr = FillSetFromVector(except, inExceptSet, nfds);
+
+	int toRet = select(nfds + 1, readPtr, writePtr, exceptPtr, nullptr);
+
+	if (toRet > 0)
+	{
+		FillVectorFromSet(outReadSet, inReadSet, read);
+		FillVectorFromSet(outWriteSet, inWriteSet, write);
+		FillVectorFromSet(outExceptSet, inExceptSet, except);
+	}
+	return toRet;
+}
+
+
